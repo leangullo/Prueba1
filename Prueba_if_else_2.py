1d5244970cad4569c7b3218419f5217c4d359d8d
Leandro <leandrosec19@gmail.com>
2020-01-22 19:38:59 -0300

    añadiendo el archivo Prueba_if_else_2.py

---
+++ Prueba_if_else_2.py
@@ -0,0 +1,8 @@
+numero_ganador = 7
+numero_usuario = int(input("Dime un numero del uno al diez: "))
+if numero_usuario == numero_ganador:
+    print("Has adivinado el numero, felicidades.")
+else:
+    numero_usuario < numero_ganador
+    numero_usuario > numero_ganador
+    print("Has perdido, lo siento mucho.")
