f75e96c8ddf4de333bb7237284b5b1e81265b905
Leandro <leandrosec19@gmail.com>
2020-01-22 19:20:51 -0300

    añadiendo el archivo prueba_if_y_else.py

---
+++ prueba_if_y_else.py
@@ -0,0 +1,8 @@
+numero_usuario = int(input("Dime un numero: "))
+if numero_usuario > 5:
+    print("Tu numero es mayor que 5")
+else:
+    numero_usuario < 5
+    print("Tu numero es menor que 5")
+print()
+print("if = si, else = sino.")
