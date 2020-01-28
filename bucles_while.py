32def9d91c5d42e094376197db9f1432a28099b7
Leandro <leandrosec19@gmail.com>
2020-01-28 00:01:28 -0300

    añadiendo el archivo bucles_while.py

---
+++ bucles_while.py
@@ -0,0 +1,12 @@
+print("los bucles while son muy parecidos a la condicion if, significa ´mientras´,"
+      "\nEn este caso, quiere decir que mientras While sea menor o igual a 30,"
+      " se repetira la sentencia print hasta que la condicion deje de ser True."
+      "\nY, a parte de eso, el While = While + 1, lo que hace es sumarle 1 cada vez que se repita"
+      "\nes decir, si no fuera por esa sentencia, se repetiria infinitamente el numero 1 "
+      "\npor ejemplo:".upper())
+print()
+
+While = 1
+while While <= 30:
+    print(While)
+    While = While + 1
